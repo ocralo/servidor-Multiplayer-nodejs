919e9c0ff49da506622b503b263c9c0350fd8835
ocralo <ocralo@gmail.com>
2020-02-11 18:22:35 -0500

    correcciones del servidor

--- server/index.js
+++ server/index.js
@@ -7,17 +7,39 @@ server.listen(7001, () => {
 });
 
 app.get("/", function(req, res) {
-  res.sendFile("../public/");
+  res.sendFile(
+    "/Volumes/Macintosh HD/Escritorio/Trabajo de grado/Pruebas Unity/servidor-Multiplayer-nodejs/public/index.html"
+  );
 });
 
 io.on("connection", function(socket) {
   console.log(socket.client.id);
+  
   socket.on("user0", function(data) {
     //console.log(data);
-    socket.emit("user0", `${JSON.stringify(data)}`);
+
+    /* socket.emit(
+      "user0",
+      `${data.playerName},${data.position.x},${data.position.y},${data.position.z},${data.life},${data.id}`
+    ); */
+    io.emit(
+      "user0",
+      `${JSON.stringify(data)
+        .replace(/\"/g, "'")
+        .replace(/"/g, "")}`
+    );
   });
   socket.on("user1", function(data) {
-    //console.log(data);
-    socket.emit("user1", `${JSON.stringify(data)}`);
+    console.log(data);
+    /* socket.emit(
+      "user1",
+      `${data.playerName},${data.position.x},${data.position.y},${data.position.z},${data.life},${1}`
+    ); */
+    io.emit(
+      "user1",
+      `${JSON.stringify(data)
+        .replace(/\"/g, "'")
+        .replace(/"/g, "")}`
+    );
   });
 });
